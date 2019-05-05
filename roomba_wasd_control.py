290d871478d65962559bc1ed8297145c967ac994
Maximilian Kempa <maximilian.kempa@gmx.de>
2019-05-05 11:59:17 +0200

    motor control, 1st version

--- roomba_wasd_control.py
+++ roomba_wasd_control.py
@@ -3,6 +3,40 @@ import readchar
 import rospy
 from std_msgs.msg import Int8
 
+## Motor Stuff
+import time
+import pigpio
+
+pi = pigpio.pi()
+
+# definition of H-Bridge IC pins
+pinDirection = 22
+pinPWM = 24
+pinDisable = 26
+
+# initialization of GPIO pins
+pi.set_mode(pinsA[0], pigpio.OUTPUT)
+pi.set_mode(pinsA[1], pigpio.OUTPUT)
+pi.set_mode(pinsA[2], pigpio.OUTPUT)
+
+
+def driveMotorForwards():
+    pi.write(pinDirection, True)
+    pi.write(pinPWM, True)
+    pi.write(pinDisable, False)
+
+
+def driveMotorBackwards():
+    pi.write(pinDirection, False)
+    pi.write(pinPWM, True)
+    pi.write(pinDisable, False)
+
+
+def stopMotor():
+    pi.write(pinDirection, False)
+    pi.write(pinPWM, False)
+    pi.write(pinDisable, True)
+
 
 def talker():
     pub = rospy.Publisher('pet_roomba3000', Int8, queue_size=10)
@@ -12,7 +46,7 @@ def talker():
         key = readchar.readkey()
         key = key.lower()
 
-        if key in ('wasd'):
+        if key in ('wasdujn'):
             if key == 'w':  # Forward
                 print "w"
                 pub.publish(1)
@@ -25,6 +59,13 @@ def talker():
             elif key == 's':  # stand still
                 print "s"
                 pub.publish(6)
+            elif key == 'u':
+                driveMotorForwards()
+            elif key == 'j':
+                driveMotorBackwards()
+            elif key == 'n':
+                stopMotor()
+
         elif key == 'p':
             print "stop"
             break
